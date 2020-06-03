206be5461eced6cbb720a680eda5829f62915f86
GavinWong <lordgavin77@gmail.com>
2020-06-03 15:15:41 +0900

    工程初始化

---
+++ idea­git­test/src/Test01.java
@@ -0,0 +1,14 @@
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+/**
+ * @author Gavin
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 15:14
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
